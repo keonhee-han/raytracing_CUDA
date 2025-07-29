e328174bcbf7a92431ab924097a9dc4d0f270289
gdbk1124 <hkh-m@w221-u-v4.eduroam.dynamic.rbg.tum.de>
2025-07-29 17:14:41 +0200

    typo fixed + converted sphere.h into CUDA compatible

--- hittable_list.h
+++ hittable_list.h
@@ -39,24 +39,24 @@ class hittable_list : public hittable {
     //     objects.push_back(object);
     // }
 
-    // hittable_list::hit() using `interval.h`
-    // bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-    __device__ bool hittable_list::hit(const ray& r_, float t_min, float t_max, hit_record& rec) const{
-        hit_record temp_rec;
-        bool hit_anything = false;
-        float closest_so_far = t_max;
-        // auto closest_so_far = ray_t.max;
+// hittable_list::hit() using `interval.h`
+// bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+__device__ bool hittable_list::hit(const ray& r_, float t_min, float t_max, hit_record& rec) const{
+    hit_record temp_rec;
+    bool hit_anything = false;
+    float closest_so_far = t_max;
+    // auto closest_so_far = ray_t.max;
 
-        // for (const auto& object : objects) {
-        for (int i_= 0; i_ < list_size; i++) {
-            if (list[i_]->hit(r_, t_min, closest_so_far, temp_rec)) {
-            // if (object->hit(r, interval(ray_t.min, closest_so_far), temp_rec)) {
-                hit_anything = true;
-                closest_so_far = temp_rec.t;
-                rec = temp_rec;
-            }
+    // for (const auto& object : objects) {
+    for (int i_= 0; i_ < list_size; i_++) {
+        if (list[i_]->hit(r_, t_min, closest_so_far, temp_rec)) {
+        // if (object->hit(r, interval(ray_t.min, closest_so_far), temp_rec)) {
+            hit_anything = true;
+            closest_so_far = temp_rec.t;
+            rec = temp_rec;
         }
-        return hit_anything;
+    }
+    return hit_anything;
 }
 
 
--- sphere.h
+++ sphere.h
@@ -16,46 +16,41 @@
 
 class sphere : public hittable {
   public:
-    sphere(const point3& center, double radius)
-      : center(center), radius(std::fmax(0,radius)) {}
+    __device__ sphere() {}
+    __device__ sphere(vec3 cen, float r) : center(cen), radius(r_) {};
     // sphere(const point3& center, double radius, shared_ptr<material> mat)
     //   : center(center), radius(std::fmax(0,radius)), mat(mat) {}
+    __device__ virtual bool hit(const ray& r_, float t_min, float t_max, hit_record& rec) const;
+    vec3 center;
+    float radius;
+};
 
   // using `interval.h`
-    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+  //bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+  __device__ bool sphere::hit(const ray& r_, float t_min, float t_max, hit_record& rec) const {
         vec3 oc = center - r.origin();
-        auto a = r.direction().length_squared();
-        auto h = dot(r.direction(), oc);
-        auto c = oc.length_squared() - radius*radius;
-
-        auto discriminant = h*h - a*c;
-        if (discriminant < 0)
-            return false;
-
-        auto sqrtd = std::sqrt(discriminant);
-
-        // Find the nearest root that lies in the acceptable range.
-        auto root = (h - sqrtd) / a;
-        if (!ray_t.surrounds(root)) {
-            root = (h + sqrtd) / a;
-            if (!ray_t.surrounds(root))
-                return false;
+        float a = dot(r.direction(), r.direction());
+        float b = dot(oc, r.direction());
+        float c = dot(oc, oc) - radius*radius;
+        float discriminant = h*h - a*c;
+        if (discriminant > 0) {
+            float temp = (-b - sqrt(discriminant))/a;
+            if (temp < t_max && temp > t_min) {
+                rec.t = temp;
+                rec.p = r.point_at_parameter(rec.t);
+                rec.normal = (rec.p - center) / radius;
+                return true;
+            }
+            temp = (-b + sqrt(discriminant)) / a;
+            if (temp < t_max && temp > t_min) {
+                rec.t = temp;
+                rec.p = r.point_at_parameter(rec.t);
+                rec.normal = (rec.p - center) / radius;
+                return true;
+            }
         }
-
-        rec.t = root;
-        rec.p = r.at(rec.t);
-        vec3 outward_normal = (rec.p - center) / radius;
-        rec.set_face_normal(r, outward_normal);
-        rec.mat = mat;
-
-        return true;
-    }
-
-  private:
-    point3 center;
-    double radius;
-    shared_ptr<material> mat;
-};
+        return false;
+}
 
 
 #endif
